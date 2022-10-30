479ecf3c3891c6121d05dbd085de1688a98ff2d1
j_ryadnova <accessyu@yandex.ru>
2022-10-30 17:29:15 +0300

    тест логин

---
+++ conftest.py
@@ -0,0 +1,97 @@
+from fixture2.application import Application
+import pytest
+import json
+import os.path
+import jsonpickle
+import importlib
+import ftputil
+
+fixture = None
+target = None
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as active_file:
+            target = json.load(active_file)
+    return target
+
+
+@pytest.fixture
+def app(request, config):
+    global fixture
+    browser = request.config.getoption("--browser")
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, config=config)
+    fixture.session.ensure_login(username=config['webadmin']["username"],
+                                 password=config['webadmin']["password"])
+    return fixture
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_config(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_config(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_config(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.back'):
+            remote.remove('config_inc.php.back')
+        if remote.path.isfile('config_inc.php'):
+            remote.rename('config_inc.php', 'config_inc.php.back')
+        remote.upload(os.path.join(os.path.dirname(__file__), 'resources/config_inc.php'), 'config_inc.php')
+
+
+def restore_server_config(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.back'):
+            if remote.path.isfile('config_inc.php'):
+                remote.remove('config_inc.php')
+            remote.rename('config_inc.php.back', 'config_inc.php')
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
+
+
+
+
--- fixture/application.py
+++ fixture/application.py
@@ -1,22 +1,54 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
-from fixture.group import GroupHelper
-from fixture.contact import ContactHelper
-class Application:
+from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
+from fixture.soap import SoapHelper
 
-    def __init__(self, browser, base_url):
+
+
+class Application:
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
             self.wd = webdriver.Chrome()
-        elif browser == "ie":
-            self.wd = webdriver.Ie()
+        elif browser == "opera":
+            self.wd = webdriver.Opera()
         else:
-            raise ValueError("Unrecognized browser %s" % browser)
+            raise ValueError("Unrecognised browser argument: %s" % browser)
+        self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
-        self.group = GroupHelper(self)
-        self.contact = ContactHelper(self)
-        self.base_url = base_url
+        self.project = ProjectHelper(self)
+        self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+        self.soap = SoapHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
+        self.edit_url = config['web']['editUrl']
+        self.soap_url = config['web']['soapUrl']
+        self.proj_page_url = config['web']['projPageUrl']
+
+
+    def open_home_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith('/addressbook/') and len(wd.find_elements_by_link_text("Last name")) > 0):
+            wd.get(self.base_url)
+
+    def open_edit_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith('/manage_proj_create_page.php/') and
+                len(wd.find_elements_by_css_selector("input[value='Add Project']")) > 0):
+            wd.get(self.edit_url)
+
+    def open_project_list(self):
+        wd = self.wd
+        if not (wd.current_url.endswith('/manage_proj_page.php') and
+                len(wd.find_elements_by_css_selector("input[value='Add Category']")) > 0):
+            wd.get(self.proj_page_url)
+
 
     def is_valid(self):
         try:
@@ -25,13 +57,6 @@ class Application:
         except:
             return False
 
-    def open_home_page(self):
-        wd = self.wd
-        wd.get(self.base_url)
 
-    def return_to_contact(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def destroy (self):
+    def destroy(self):
         self.wd.quit()
